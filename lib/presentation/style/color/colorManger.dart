2daaa221b9fbb3d5faf3aca363c24c888103331b
mohamedradwan96 <eng.mohamedradwan996@gmail.com>
2023-05-31 17:42:07 +0300

    template of clean code

---
+++ lib/presentation/style/color/colorManger.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ColorManager {
+  static Color primary = const Color(0xBD000000);
+  static Color sec = const Color(0xefffb701);
+  static Color button = const Color(0xefffb701);
+  // static Color primary = const Color(0xff484546);
+  // static Color primary = const Color(0xff484546);
+  // static Color primary = const Color(0xff484546);
+  // static Color primary = const Color(0xff484546);
+  // static Color primary = const Color(0xff484546);
+}
