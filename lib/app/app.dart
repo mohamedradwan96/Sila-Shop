8a8c66d3e158055ac3be842d6b92c65714905218
mohamedradwan96 <eng.mohamedradwan996@gmail.com>
2023-05-31 17:39:39 +0300

    template of clean code

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  static final MyApp _instance =
+      MyApp._internal(); // single instance in all app
+  factory MyApp() => _instance; // factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
